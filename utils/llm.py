19f78eabf714becdafcf68a603d31c632c68afc4
ankithsavio <ankithsavio@gmail.com>
2025-05-23 19:15:20 +0100

    add gemini client to get reasoning outputs

--- utils/llm.py
+++ utils/llm.py
@@ -1,7 +1,9 @@
 import os
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional
 
 import openai
+from google import genai
+from google.genai import types
 from tenacity import (
     retry,
     retry_if_exception_type,
@@ -95,3 +97,98 @@ class llm(limiter):
         self.increment_count()
 
         return response
+
+
+class reasoningllm(limiter):
+    def __init__(
+        self,
+        model: str = "gemini-2.5-flash-preview-04-17",
+        api_key: Optional[str] = os.getenv("GOOGLE_API_KEY", None),
+        req_per_day: int = 500,
+    ):
+        """
+        Args:
+            model (str) : model name
+            api_key (str) : api key for the provider
+            req_per_day (int) : rate limit from the provider
+
+        Defaults to use Gemini 2.5 pro with 150 req/day
+        """
+        super().__init__(req_per_day=req_per_day)
+
+        if not api_key:
+            raise ValueError("API Key not set")
+
+        self.client = genai.Client(api_key=api_key)
+
+        # Can add hyper parameters with the config
+        self.input_config = {
+            "model": model,
+            "contents": None,
+        }
+        self.config = types.GenerateContentConfig(
+            thinking_config=types.ThinkingConfig(include_thoughts=True)
+        )
+        self.messages = []
+
+    @retry(
+        retry=retry_if_exception_type(
+            (openai.RateLimitError, openai.InternalServerError)
+        ),
+        wait=wait_random_exponential(min=1, max=10),
+        stop=stop_after_attempt(3),
+        reraise=True,
+        retry_error_callback=lambda retry_state: None,
+    )
+    def generate(self, messages: List[Dict[str, Any]]) -> Optional[list[types.Part]]:
+        system_instruction = None
+        message_contents = []
+        for message in messages:
+            if message["role"] == "system":
+                system_instruction = message["content"]
+            else:
+                message_contents.append(message["content"])
+
+        try:
+            response = self.client.models.generate_content(
+                model="gemini-2.5-flash-preview-05-20",
+                contents=message_contents,
+                config=types.GenerateContentConfig(
+                    thinking_config=types.ThinkingConfig(include_thoughts=True),
+                    system_instruction=system_instruction,
+                ),
+            )
+
+            return response.candidates[0].content.parts  # type: ignore
+
+        except openai.RateLimitError:
+            raise
+        except openai.InternalServerError:
+            raise
+
+    def __call__(self, input_prompt: str, **kwargs) -> Optional[Dict[str, Any]]:
+        if not self.check_limit:
+            print("Rate limit reached")
+            return None
+
+        self.messages.append({"role": "user", "content": input_prompt})
+
+        response_parts = self.generate(self.messages)
+
+        if not response_parts:
+            return None
+
+        reasoning_response = None
+        response = None
+
+        for part in response_parts:
+            if not part.text:
+                continue
+            if part.thought:
+                reasoning_response = part.text
+            else:
+                response = part.text
+
+        self.increment_count()
+
+        return {"reasoning": reasoning_response, "response": response}
